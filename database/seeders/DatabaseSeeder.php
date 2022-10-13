3343b921fa010c2a90bc81f9ae3daeae3aac6e96
RiadBerber-76800 <riadberber@gmail.com>
2022-10-13 10:14:56 +0200

    Factory+seed+migration ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,25 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+ /**
+  * Seed the application's database.
+  *
+  * @return void
+  */
+ public function run()
+ {
+  User::factory(5)->create();
+  Video::factory(20)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+  // \App\Models\User::factory()->create([
+  //     'name' => 'Test User',
+  //     'email' => 'test@example.com',
+  // ]);
+ }
 }
